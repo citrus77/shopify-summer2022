888e1984fb7adfb12442445dacfc4156aa4febd1
citrus77 <jason.lammers@outlook.com>
2022-05-19 23:38:13 -0700

    more styling to response cards

--- src/components/SingleResp.js
+++ src/components/SingleResp.js
@@ -4,10 +4,10 @@ import './css/SingleResp.css';
 const SingleResp = ({ response }) => {
     const { date, prompt, resp } = response;
     return (
-        <div className='resp'>
+        <div className='resp-card'>
             <span className='date'>{ date }</span>
             <span className='prompt'><h4>Prompt: </h4>{ prompt }</span>
-            <span><h4>Response: </h4>{ resp }</span>
+            <span className='resp'><h4>Response: </h4>{ resp }</span>
         </div>
     );
 }

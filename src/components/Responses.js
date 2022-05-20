3d6fdb9e136936575976cf49ac6341892e95c47e
citrus77 <jason.lammers@outlook.com>
2022-05-20 13:39:43 -0700

    added delete button to resp cards

--- src/components/Responses.js
+++ src/components/Responses.js
@@ -3,17 +3,21 @@ import { uid } from 'react-uid';
 import './css/Responses.css';
 import { SingleResp } from '.';
 
-const Responses = ({ responses }) => {
-    if (responses) {
+const Responses = ({ responses, setResponses }) => {
+    if (responses && responses.length > 0) {
         return (<div id='resp-container'>{
             responses.map(response => <SingleResp
-                key={uid(response)}
-                response={response}
+                key={ uid(response) }
+                response={ response }
+                responses={ responses }
+                setResponses={ setResponses }
             />) 
             .reverse()        
         }</div>);
     } else {
-        return (<div id='resp-container'>No responses yet</div>);
+        return (<div id='resp-container'>
+                <span>No responses yet</span>
+        </div>);
     } 
 }; 
 
--- src/components/SingleResp.js
+++ src/components/SingleResp.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './css/SingleResp.css';
 
-const SingleResp = ({ response }) => {
+const SingleResp = ({ response, responses, setResponses }) => {
     const { date, engine, prompt, resp } = response;
+
+    const _handleDelete = (e) => {
+        setResponses((responses) => responses.filter(r => r !== response))
+    };
+
     return (
         <div className='resp-card'>
             <div className='resp-card-info'>
@@ -12,6 +17,9 @@ const SingleResp = ({ response }) => {
             
             <span className='prompt'><h4>Prompt: </h4>{ prompt }</span>
             <span className='resp'><h4>Response: </h4>{ resp }</span>
+            <button className='delete-button' onClick={ _handleDelete }>
+                <img src='./images/trash.png' height='32' width='32' alt='Delete' />
+            </button>
         </div>
     );
 }
